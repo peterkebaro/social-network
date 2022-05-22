c57d8e628673e5abfd4e2171813565cf8e44f15b
Peter Rodriguez <peterkebaro@gmail.com>
2022-05-22 19:53:59 +0100

    readme.md and try other characteristics

--- src/pruebas/cars.tsx
+++ src/pruebas/cars.tsx
@@ -1,45 +1,94 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
 
-interface CarsProps{
-    cars: {
-        marca: string
-        precio: string
-    }[]
+interface CarsState {
+    brand: string;
+    price: number;
 }
 
-interface CarsState{
-    marca: string
-    precio: string
+interface Data {
+    car: string;
+    priceCar: number;
 }
-
-export class Cars extends Component <CarsProps, CarsState>{
-
-    constructor (props: CarsProps) {
-
-        super(props)
+interface CarsProps {
+    carsData: Data[];
+}
+export class Cars extends Component<CarsProps, CarsState> {
+    constructor(props: CarsProps) {
+        super(props);
         this.state = {
-            marca: '',
-            precio: ''
-        }
-
+            brand: "",
+            price: 0,
+        };
     }
 
+    addNewCar() {
+        const newCar: Data = {
+            car: this.state.brand,
+            priceCar: this.state.price,
+        };
+
+        this.props.carsData.push(newCar);
+
+        this.setState({
+            brand: "",
+            price: 0,
+        });
+    }
+
+    deleteCar() {}
+
+    editCar() {}
 
     render() {
+        const { carsData } = this.props;
 
-        const {cars} = this.props
-
-        return(
-            
+        return (
             <div>
-                <h1>Stock de Coches</h1>
-                <input 
-                    placeholder='Marca del coche'
-                    value={this.state.marca}
-                    onChange={ (event) => { this.setState({ marca: event.target.value })}}
-                /><br/>
-
+                <br />
+                <h1>Stock de coches</h1>
+                <br />
+                <div>
+                    <input
+                        placeholder="Car Brand"
+                        value={this.state.brand}
+                        onChange={(event) =>
+                            this.setState({ brand: event.target.value })
+                        }
+                    />
+                    <br />
+                    <input
+                        placeholder="Car Price"
+                        type="number"
+                        value={this.state.price}
+                        onChange={(event) =>
+                            this.setState({ price: event.target.valueAsNumber })
+                        }
+                    />
+                    <br />
+                    <button onClick={() => this.addNewCar()}>
+                        {" "}
+                        Register Car
+                    </button>
+                    <br />
+                </div>
+                <div className="stockCarList">
+                    Stock List: <br />
+                    {carsData.map((element) => (
+                        <li key={element.car}>
+                            {" "}
+                            Brand: {element.car} - Price: {element.priceCar}{" "}
+                            euros
+                            <button onClick={() => this.editCar()}>
+                                Editar
+                            </button>
+                            <button onClick={() => this.deleteCar()}>
+                                Borrar
+                            </button>
+                        </li>
+                    ))}
+                    <br />
+                </div>
             </div>
-        )
+        );
     }
 }
